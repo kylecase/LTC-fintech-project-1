1c486eeb924e6c68a1351617bc11c2af82e2f946
Kyle Case <kcase@hey.com>
2022-08-22 18:50:56 -0500

    Limit request to 100

--- portfolio-optimizer/src/constants.ts
+++ portfolio-optimizer/src/constants.ts
@@ -1,3 +1,3 @@
 export const API = {
-  COVALENT_URL: `https://api.covalenthq.com/v1/1/xy=k/uniswap_v2/tokens/?quote-currency=USD&format=JSON&page-size=1000000000&key=${process.env.REACT_APP_COVALENT_API_KEY}`,
+  COVALENT_URL: `https://api.covalenthq.com/v1/1/xy=k/uniswap_v2/tokens/?quote-currency=USD&format=JSON&page-size=100&key=${process.env.REACT_APP_COVALENT_API_KEY}`,
 };
